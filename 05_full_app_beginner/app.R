f87af7dc50b06b847a9762e0a91ebaa83b3620d2
Jesse Mostipak <jesse.maegan@gmail.com>
2021-09-25 20:34:05 -0500

    completed basic Palmer Penguin app

--- 05_full_app_beginner/app.R
+++ 05_full_app_beginner/app.R
@@ -1,21 +1,25 @@
 # set up ----
 library(shiny)
-library(dplyr) ## may not need
+library(stringr)
 library(ggplot2)
+library(readr)
 library(DT)
+library(tools)
 library(palmerpenguins)
 
 # user interface ====
 ui <- fluidPage(
+  fillPage(padding = c(20, 100, 20, 100)),
   
-  titlePanel("Palmer Penguins basic app"),
+  title = "Palmer Penguin app",
+  
+  titlePanel(tags$img(src = "https://allisonhorst.github.io/palmerpenguins/man/figures/palmerpenguins.png", height = "100px", " Palmer Penguins basic app")),
   
   sidebarLayout(
     
     sidebarPanel(
       
       # select x-axis value for scatterplot ----
-      #tags$h2("Scatterplot"),
       selectizeInput(inputId = "x_axis",
                   label = "Select a value for the x-axis: ",
                   choices = c(
@@ -63,18 +67,25 @@ ui <- fluidPage(
                   min = 0, max = 1, value = 0.8, 
                   step = 0.1),
       
-      #tags$h2("Data table"),
-      # filter by species here ====
-      # using for both scatterplot and table -- will create a reactive function
+      # filter by species  ====
       checkboxGroupInput(inputId = "penguin_species",
                          label = "Filter by species: ",
                          choices = c("Adelie", "Chinstrap", "Gentoo"),
                          selected = c("Adelie", "Chinstrap", "Gentoo")),
       
+      # add a plot title ====
+      textInput(inputId = "plot_title",
+                label = "Plot title",
+                placeholder = "Create a title for your plot"),
+      
       # show or hide data table ====
       checkboxInput(inputId = "show_table",
                     label = "Check box to show data table",
-                    value = TRUE)
+                    value = TRUE),
+      
+      # write data to csv ----
+      downloadButton(outputId = "download_csv",
+                     label = "Download CSV")
       
     ),
     
@@ -82,12 +93,17 @@ ui <- fluidPage(
       
       # summary text
       htmlOutput(outputId = "summary_text"),
+      br(),
+      
+      # plot title
+      htmlOutput(outputId = "plot_description"),
       
       # scatterplot (ui) ----
-      plotOutput(outputId = "penguin_scatterplot")),
+      plotOutput(outputId = "penguin_scatterplot"),
       
       # datatable (ui) ----
-      DTOutput(outputId = "penguin_datatable")
+      DTOutput(outputId = "penguin_datatable"),
+      
     )
   )
 )
@@ -97,17 +113,24 @@ server <- function(input, output, session) {
   
   # scatterplot (server) ----
   
-  # reactive function for penguin species
+  # reactive function for penguin species ----
   filtered_penguins <- reactive({
     req(input$penguin_species)
     filter(penguins, species %in% input$penguin_species)
   })
   
+  # # reactive function for plot title ----
+  title_case_plot <- reactive({
+    tags$h3(toTitleCase(input$plot_title))
+  })
+
+  # summary text ----
   output$summary_text <- renderUI({
-    tags$h3("You're viewing data for ", nrow(filtered_penguins()), 
+    tags$h4("You're viewing data for ", nrow(filtered_penguins()), 
             "Palmer penguins")
   })
   
+  # scatterplot ----
   output$penguin_scatterplot <- renderPlot({
     
     # req() to remove error when nothing is selected
@@ -124,6 +147,9 @@ server <- function(input, output, session) {
                  size = input$point_size,
                  alpha = input$point_alpha) +
       scale_color_manual(values = c("darkorange", "darkorchid", "cyan4")) +
+      # point out why not using data masking
+      labs(x = str_replace_all(input$x_axis, "_", " "),
+           y = str_replace_all(input$y_axis, "_", " ")) +
       theme_bw()
   })
   
@@ -135,6 +161,27 @@ server <- function(input, output, session) {
                     options = list(pageLength = 10))
     }
   })
+  
+  # plot title ----
+  # output$plot_description <- renderText({
+  #   title_case_plot()
+  # })
+  output$plot_description <- renderUI({
+    title_case_plot()
+  })
+  
+  
+  # download .csv of filtered data ----
+  output$download_csv <- downloadHandler(
+    filename = function() {
+      paste0("penguins_",
+             str_replace_all(Sys.time(), ":|\ ", "_"),
+             ".csv")
+    },
+    content = function(file) {
+      write_csv(filtered_penguins(), file)
+    }
+  )
 }
 
 # run Shiny app ====
